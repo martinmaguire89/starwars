7864d2750b974508551ab6b23fb1b2297a3ee61d
Ubuntu <ubuntu@ip-172-31-88-237.ec2.internal>
2019-11-04 15:47:45 +0000

    implement pagination

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
- const baseUrl = "https://swapi.co/api/";
- 
- function getData(type,cb){
-     var xhr = new XMLHttpRequest();
-
-    xhr.open("GET",baseUrl + type + "/");
-    xhr.send();
+function getData(url, cb) {
+    var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
- }
- 
- function getTableHeader(obj){
-     var tableHeaders = [];
-     
-     Object.keys(obj).forEach(function(key){
-            tableHeaders.push(`<td>${key}</td>`);
-            });
-            
-            return `<tr>${tableHeaders}</tr>`;
- }
- 
-function writeToDocument(type){
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
-    el.innerHTML = "";
-    
-    getData(type, function(data){
+
+    getData(url, function(data) {
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
-        var tableHeaders = getTableHeader(data[0]);
-        
-        data.forEach(function(item){
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
             var dataRow = [];
-            
-            Object.keys(item).forEach(function(key){
+
+            Object.keys(item).forEach(function(key) {
                 var rowData = item[key].toString();
-                var truncatedData = rowData.substring(0, 15)
+                var truncatedData = rowData.substring(0, 15);
                 dataRow.push(`<td>${truncatedData}</td>`);
             });
-            tableRows.push(`<td>${dataRow}</td>`);
+            tableRows.push(`<tr>${dataRow}</tr>`);
         });
-        
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
-       
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
